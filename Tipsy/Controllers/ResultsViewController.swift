60f6af66b89ddd7a89907fb75fd47545ee3bf096
gred-wasntavailable <griffin_reding@hotmail.com>
2022-07-27 09:42:46 -0700

    Updated file structure

--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -26,16 +26,4 @@ class ResultsViewController: UIViewController {
     @IBAction func recalculatePressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
